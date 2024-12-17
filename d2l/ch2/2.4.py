3cc95663454cd655eaa3079c896162645f794a26
kaishaoshao <2032751077@qq.com>
2024-12-17 17:26:41 +0800

    finish 2.5.py && finish 2.7.py

---
+++ d2l/ch2/2.4.py
@@ -0,0 +1,8 @@
+# 微积分
+# 导数和微分
+import matplotlib.pyplot as plt
+import numpy as np
+from d2l import torch as d2l
+
+def f(x):
+    return 3 * x ** 2 - 4 * x
---
+++ d2l/ch2/2.7.py
@@ -0,0 +1,10 @@
+# 查阅文档
+
+# 查找模块中的所有函数和类
+import torch
+print(dir(torch.distributions))
+
+# 查找特定函数和类的用法
+help(torch.ones)
+print(torch.ones(2, 3))
+print(torch.ones(5))
