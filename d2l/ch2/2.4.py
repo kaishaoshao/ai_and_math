46077f3341b9e717c99d6c0923adb0dcc51ae87c
kaishaoshao <2032751077@qq.com>
2024-12-18 09:43:58 +0800

    2.4.py 失败后面再研究

--- d2l/ch2/2.4.py
+++ d2l/ch2/2.4.py
@@ -6,3 +6,50 @@ from d2l import torch as d2l
 
 def f(x):
     return 3 * x ** 2 - 4 * x
+
+def numerical_lim(f, x, h):
+    return (f(x + h) - f(x)) / h
+
+h = 0.1
+for i in range(5):
+    print(f'h={h:.5f}, numerical limit={numerical_lim(f, 1, h):.5f}')
+    h *= 0.1
+
+def use_svg_display():
+    d2l.set_figsize()
+    # 用矢量图显示
+    d2l.plt.switch_backend('svg')
+    #plt.set_matplotlib_formats('svg')
+
+# 定义set_figsize函数来设置图表的大小
+def set_figsize(figsize=(3.5, 2.5)):
+    use_svg_display()
+    # 设置图的尺寸
+    d2l.plt.rcParams['figure.figsize'] = figsize
+
+def set_axes(axes, xlabel, ylabel, xlim, ylim, xscale, yscale, legend):
+    axes.set_xlabel(xlabel)
+    axes.set_ylabel(ylabel)
+    axes.set_xscale(xscale)
+    axes.set_yscale(yscale)
+    axes.set_xlim(xlim)
+    axes.set_ylim(ylim)
+    if legend:
+        axes.legend(legend)
+    axes.grid()
+
+def plot(X, Y=None, xlabel=None, ylabel=None, legend=None, xlim=None,
+    ylim = None, xscale = 'linear', yscale = 'linear'
+    fmts = ('-', 'm--', 'g-.', 'r:'),figsize = (3.5, 2.5), axes = None):
+    # 绘制
+    if legend is None:
+        legend = []
+    set_figsize(figsize)
+    axes = axes if axes else d2l.plt.gca()
+
+# 如果X有一个轴，输出True
+def has_one_axis(X):
+    return (hasattr(X, "ndim") and X.ndim == 1 or isinstance(X, list)
+and not hasattr(X[0], "__len__"))
+
+
